2e20ebfdf0e661cdf7d0500df38e6c02d3e542d0
Gian Akbar <akbarigi20@gmail.com>
2021-09-15 18:37:33 +0700

    module

--- week-4-oop-javascript/export-module/src/javascript/app.js
+++ week-4-oop-javascript/export-module/src/javascript/app.js
@@ -1,3 +1,18 @@
-import app from "./table.js"
+import {Table} from "./table.js"
 
-app()
+const table = new Table({
+    columns: ["Name", "Age", "Birthday"],
+    data: [
+        ["Gian", "20", "11/10/2000"],
+        ["testing", "22", "11/10/2000"],
+        ["Aku", "30", "11/10/2000"],
+        ["kita", "30", "11/10/2000"],
+        ["dia", "30", "11/10/2000"],
+        ["mereka", "30", "11/10/2000"],
+    ]
+})
+const table1 = document.getElementById("table")
+table.render(table1)
+
+const table2 = document.getElementById("table2")
+table.render(table2)
--- week-4-oop-javascript/export-module/src/javascript/table.js
+++ week-4-oop-javascript/export-module/src/javascript/table.js
@@ -1,53 +1,43 @@
 class Table {
-    constructor(init) {
-      this.init = init;
-    }
-  
-    createTableHeader(data) {
-      let tagOpen = "<thead><tr>";
-      let tagCloses = "</tr></thead>";
-      data.forEach((d) => {
-        tagOpen += `<th>${d}</th>`;
-      });
-  
-      return tagOpen + tagCloses;
-    }
-  
-    createTableBody(data) {
-      let tagOpen = "<tbody>";
-      let tagCloses = "</tbody>";
-  
-      data.forEach((data) => {
-        tagOpen += `
+  constructor(init) {
+    this.init = init
+  }
+
+  createTableHeader(data) {
+    let tagOpen = "<thead><tr>"
+    let tagCloses = "</tr></thead>"
+    data.forEach((d) => {
+      tagOpen += `<th>${d}</th>`
+    })
+
+    return tagOpen + tagCloses
+  }
+
+  createTableBody(data) {
+    let tagOpen = "<tbody>"
+    let tagCloses = "</tbody>"
+
+    data.forEach((data) => {
+      tagOpen += `
           <tr>
             <td>${data[0]}</td>
             <td>${data[1]}</td>
             <td>${data[2]}</td>
           </tr>
-        `;
-      });
-  
-      return tagOpen + tagCloses;
-    }
-  
-    render(element) {
-      let table =
-        "<table class='table table-hover'>" +
-        this.createTableHeader(this.init.columns) +
-        this.createTableBody(this.init.data) +
-        "</table>";
-      element.innerHTML = table;
-    }
+        `
+    })
+
+    return tagOpen + tagCloses
+  }
+
+  render(element) {
+    let table =
+      "<table class='table table-hover'>" +
+      this.createTableHeader(this.init.columns) +
+      this.createTableBody(this.init.data) +
+      "</table>"
+    element.innerHTML = table
+  }
 }
-
-const table = new Table({
-    columns: ["Name", "Email", "Phone"],
-    data: [
-        ["Gian", "gian@gmail.com", 62982324],
-        ["testing", "testing@testing.com", 2343242],
-        ["Aku", "test@gmail.com", 628333]
-    ]
-});
-
-const app = document.getElementById("table");
-export default app = table.render(app);
+ 
+export {Table}
