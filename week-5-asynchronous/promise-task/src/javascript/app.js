85b703d9cd313146eb9ddf9f55980bdb918dfb5b
Gian Akbar <akbarigi20@gmail.com>
2021-09-19 06:09:27 +0700

    edit app js

--- week-5-asynchronous/promise-task/src/javascript/app.js
+++ week-5-asynchronous/promise-task/src/javascript/app.js
@@ -1,4 +1,3 @@
-
 axios.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=feaf2358982f4325b903d09fb7a95e7a")
     .then(function (response) {
         response.data.articles.map((data) => {
@@ -27,39 +26,3 @@ axios.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=feaf2358982f43
         console.log("Succes")
     })
 
-    const search = document.getElementById("search");
-    const results = document.getElementById("results");
-    let search_term = "";
-    
-    const showList = () => {
-      results.innerHTML = "";
-      newsdata
-        .filter((item) => {
-          return (
-            item.title.toLowerCase().includes(search_term) ||
-            item.description.toLowerCase().includes(search_term)
-          );
-        })
-        .forEach((e) => {
-            let row = `
-            <div class="col">
-                <div class="card m-2">
-                    <img clas="card-img" width="100%" height="100%" src="${e.urlToImage}" >
-                    <div class="card-body">
-                    <h4 class="card-title">${e.title}</h4>
-                    <p class="card-text">${e.description}</p>
-                    <a href="${e.url}" class="btn btn-primary">Read more</a>
-                    </div>
-                </div>
-            </div>
-            `
-            card.innerHTML += row
-        });
-    };
-    
-    showList();
-    
-    search.addEventListener("input", (event) => {
-      search_term = event.target.value.toLowerCase();
-      showList();
-    });
